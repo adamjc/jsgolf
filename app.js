5852c202c3be688dd52423d588fb5349fa1d91b1
Adam Cook <adam.cook@bbc.co.uk>
2015-08-10 14:37:22 +0100

    Started working on integrating react

--- app.js
+++ app.js
@@ -5,108 +5,115 @@ var Q = require('Q');
 var app = express();
 var _ = require('lodash');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Test',
-        message: 'Hello World!'
-    });
+    res.sendFile(__dirname + '/react/index.html');
 });
 
-app.get('/exercise/:exercise/', (req, res) => {
-    var exerciseData = getExerciseData(req.params.exercise);
-
-    if (exerciseData) {
-        res.render('exercise', { exerciseData: exerciseData });
-    } else {
-        res.status(404).send('404: Nah m8.');
-    }
-});
-
-app.get('/exercise', (req, res) => {
-    var exercises = []
-    Object.keys(exerciseMap).forEach((exercise) => {
-        var fetchedExercise = require('./exercises/' + exerciseMap[exercise]);
-        fetchedExercise.url = '/exercise/' + fetchedExercise.number;
-
-        exercises.push(fetchedExercise);
-    });
-
-    res.render('exercises', {title: 'Exercises', message: 'Exercises', exercises: exercises});
-});
-
-app.post('/exercise/:exercise/', (req, res) => {
-    var exerciseData = getExerciseData(req.params.exercise);
-
-    if (!exerciseData) {
-        res.status(500).send('500: Nah m8.');
-        return;
-    }
-
-    var sandbox = new Sandbox();
-    var results = [];
-    var tests = exerciseData.tests;
-    var processData = () => {
-        var promises = Q();
-
-        Object.keys(tests).forEach((testDataElement, index) => {
-            promises = promises.then(() => {
-                var deferred = Q.defer();
-
-                var func = '(function(){ \
-                                var input = testInput; \
-                                var output = usersFunction(input); \
-                                var result = { \
-                                    "output" : output, \
-                                    "input" : input, \
-                                }; \
-                                return "" + JSON.stringify(result); + "" \
-                            })()';
-
-                var usersFunction = req.body.testFunc;
-                var newFunc;
-                newFunc = func.replace('usersFunction', usersFunction);
-
-                var testInput = tests[index].testInput;
-
-                testInput = wrapWithQuotesIfString(testInput);
-                newFunc = newFunc.replace('testInput', testInput);
-
-                sandbox.run(newFunc, (output) => {
-                    var result;
-                    result = output.result.split('');
-                    result.pop();
-                    result.shift();
-                    result = result.join('');
-
-                    var resultObject = {};
-                    try {
-                        resultObject = JSON.parse(result);
-                        resultObject.correct = _.isEqual(resultObject.output, exerciseData.tests[index].expectedOutput);
-                    } catch (e) {
-                        console.log('Error:', e);
-                        resultObject.output = 'Error';
-                    }
-
-                    results[index] = resultObject;
-                    deferred.resolve();
-                });
-
-                return deferred.promise;
-            });
-        });
-
-        return promises;
-    };
-
-    processData().then(() => {
-        res.send(results);
-    });
-});
+// app.get('/', (req, res) => {
+//     res.render('index', {
+//         title: 'Test',
+//         message: 'Hello World!'
+//     });
+// });
+//
+// app.get('/exercise/:exercise/', (req, res) => {
+//     var exerciseData = getExerciseData(req.params.exercise);
+//
+//     if (exerciseData) {
+//         res.render('exercise', { exerciseData: exerciseData });
+//     } else {
+//         res.status(404).send('404: Nah m8.');
+//     }
+// });
+//
+// app.get('/exercise', (req, res) => {
+//     var exercises = []
+//     Object.keys(exerciseMap).forEach((exercise) => {
+//         var fetchedExercise = require('./exercises/' + exerciseMap[exercise]);
+//         fetchedExercise.url = '/exercise/' + fetchedExercise.number;
+//
+//         exercises.push(fetchedExercise);
+//     });
+//
+//     res.render('exercises', {title: 'Exercises', message: 'Exercises', exercises: exercises});
+// });
+//
+// app.post('/exercise/:exercise/', (req, res) => {
+//     var exerciseData = getExerciseData(req.params.exercise);
+//
+//     if (!exerciseData) {
+//         res.status(500).send('500: Nah m8.');
+//         return;
+//     }
+//
+//     var sandbox = new Sandbox();
+//     var results = [];
+//     var tests = exerciseData.tests;
+//     var processData = () => {
+//         var promises = Q();
+//
+//         Object.keys(tests).forEach((testDataElement, index) => {
+//             promises = promises.then(() => {
+//                 var deferred = Q.defer();
+//
+//                 var func = '(function(){ \
+//                                 var input = testInput; \
+//                                 var output = usersFunction(input); \
+//                                 var result = { \
+//                                     "output" : output, \
+//                                     "input" : input, \
+//                                 }; \
+//                                 return "" + JSON.stringify(result); + "" \
+//                             })()';
+//
+//                 var usersFunction = req.body.testFunc;
+//                 var newFunc;
+//                 newFunc = func.replace('usersFunction', usersFunction);
+//
+//                 var testInput = tests[index].testInput;
+//
+//                 testInput = wrapWithQuotesIfString(testInput);
+//                 newFunc = newFunc.replace('testInput', testInput);
+//
+//                 sandbox.run(newFunc, (output) => {
+//                     var result;
+//                     result = output.result.split('');
+//                     result.pop();
+//                     result.shift();
+//                     result = result.join('');
+//
+//                     var resultObject = {};
+//                     try {
+//                         resultObject = JSON.parse(result);
+//                         resultObject.correct = _.isEqual(resultObject.output, exerciseData.tests[index].expectedOutput);
+//                     } catch (e) {
+//                         console.log('Error:', e);
+//                         resultObject.output = 'Error';
+//                     }
+//
+//                     results[index] = resultObject;
+//                     deferred.resolve();
+//                 });
+//
+//                 return deferred.promise;
+//             });
+//         });
+//
+//         return promises;
+//     };
+//
+//     processData().then(() => {
+//         res.send(results);
+//     });
+// });
 
 /**
  * As we are replacing strings, when doing so, we have to wrap it
