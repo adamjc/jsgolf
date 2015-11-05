a549542604d7a4bf5e806e028c89277fbcfcc839
Adam Cook <adam.cook@bbc.co.uk>
2015-11-05 14:51:30 +0000

    Update Exercise 2

--- server/exercises/exercise_2_add-one.js
+++ server/exercises/exercise_2_add-one.js
@@ -3,20 +3,17 @@ var addOne = {
     title: "Exercise 2 - Add One",
     description: "Produce a function that takes a Number, and adds 1 to it.",
     tests: [{
-        'testInput': 0,
-        'expectedOutput': 1
+        'testInput': 1,
+        'expectedOutput': 2
     }, {
         'testInput': -1,
         'expectedOutput': 0
     }, {
-        'testInput': 29,
-        'expectedOutput': 30
+        'testInput': 0.5,
+        'expectedOutput': 1.5
     }, {
-        'testInput': 1.5,
-        'expectedOutput': 2.5
-    }, {
-        'testInput': -1,
-        'expectedOutput': 0
+        'testInput': 999,
+        'expectedOutput': 1000
     }, {
         'testInput': 'Hello World!',
         'expectedOutput': null
