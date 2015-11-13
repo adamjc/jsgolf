a2e99ffb88f67371d6006f2c0d3e665ccf93fe51
Adam Cook <adam.cook@bbc.co.uk>
2015-11-13 14:44:31 +0000

    Add exercise 5

---
+++ server/exercises/exercise_5_parameters.js
@@ -0,0 +1,15 @@
+const maps = {
+    number: 5,
+    title: "Exercise 5 - Parameters",
+    description: "Produce a function that takes two numbers and adds them together",
+    tests: [{
+        'testInput': {
+            a: 1,
+            b: 2,
+            c: 3
+        },
+        'expectedOutput': [1, 2, 3]
+    }]
+};
+
+module.exports = maps;
