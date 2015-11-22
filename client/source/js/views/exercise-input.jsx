027f4ff001487813f0e96e99ee69bdcd813e9a15
Adam <adamjc86@gmail.com>
2015-11-22 12:37:45 +0000

    Limit sending of exercises

--- client/source/js/views/exercise-input.jsx
+++ client/source/js/views/exercise-input.jsx
@@ -5,7 +5,9 @@ const ReactAddons = require('react-addons');
 const brace = require('brace');
 const AceEditor = require('react-ace');
 const ResultActions = require('../actions/result-actions');
+const ResultStore = require('../stores/result-store');
 const ExerciseActions = require('../actions/exercise-actions');
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 require('brace/mode/javascript');
 require('brace/theme/monokai');
@@ -17,15 +19,44 @@ module.exports = React.createClass({
         };
     },
 
+    componentDidMount() {
+        ResultStore.listen(this.onResult);
+    },
+
+    componentWillUnmount() {
+        ResultStore.unlisten(this.onResult);
+    },
+
     onChange(answer) {
-        this.state.answer = answer;
+        this.setState({
+            answer: answer
+        });
+    },
+
+    onResult() {
+        this.setState({
+            awaitingResults: false
+        });
     },
 
     handleClick() {
         ResultActions.fetchResults(this.props.exercise, this.state.answer);
+
+        this.setState({
+            awaitingResults: true
+        });
     },
 
     render() {
+        let awaitingResults;
+
+        if (this.state.awaitingResults) {
+            awaitingResults =
+                    <div className="exercise-input__sent">
+                        <span className="glyphicon glyphicon-refresh" aria-hidden="true"></span>
+                    </div>
+        }
+
         return (
             <div className="exercise-input col-sm-6">
                 <AceEditor
@@ -36,14 +67,27 @@ module.exports = React.createClass({
                     height=""
                     width=""
                     onChange={this.onChange}
-                />
+                    value={this.state.answer}
+                    />
 
-                <button
-                    className="exercise-input__button btn btn-block"
-                    onClick={this.handleClick}>
-                    GO
-                </button>
+                <div className="exercise-input__button-wrapper">
+                    <button
+                        className="exercise-input__button btn btn-block"
+                        onClick={this.handleClick}>
+                        GO
+                    </button>
+
+                    <ReactCSSTransitionGroup
+                        transitionName="exericse-input-transition"
+                        transitionAppear={true}
+                        transitionAppearTimeout={500}
+                        transitionEnterTimeout={500}
+                        transitionLeaveTimeout={300}
+                    >
+                        {awaitingResults}
+                    </ReactCSSTransitionGroup>
+                </div>
             </div>
-        )
+        );
     }
 });
