8686082de2ff734c31a10e1ecb067b5050aa2e2f
Adam Cook <adam.cook@bbc.co.uk>
2015-12-31 11:26:18 +0000

    Add beginnings of passport js login authentication

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const _ = require('lodash');
 const path = require('path');
 const events = require('events');
 const socket = require('socket.io');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const getExerciseList = require('./api/getExercisesList');
 const getExercise = require('./api/getExercise');
@@ -16,9 +18,36 @@ const postRegister = require('./api/postRegister');
 let server;
 let io;
 
+passport.use(new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect username.' });
+        if (!user.validPassword(password)) {
+            return done (null, false, { message: 'Incorect password.' });
+        }
+        return done(null, user);
+    }));
+}))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(path.resolve(__dirname, '../public')));
+
+app.post('/login', passport.authenticate('local'), (req, res) => {
+    res.redirect('/user/' + req.user.username);
+})
 
 /* Returns the list of exercises available. */
 app.get('/api/exercises', getExerciseList);
