233f7aedffeda6c853b0815582dfdb2649cc4123
Adam <adamjc86@gmail.com>
2016-01-07 16:15:23 +0000

    Remove footer

--- client/source/js/app.jsx
+++ client/source/js/app.jsx
@@ -2,7 +2,6 @@
 
 const React = require('react');
 const Header = require('./header.jsx');
-const Footer = require('./footer.jsx');
 const Router = require('./router.jsx');
 const ReactDOM = require('react-dom')
 
@@ -15,8 +14,6 @@ var App = React.createClass({
                 <div className="main-container container">
                     <Router />
                 </div>
-
-                <Footer />
             </div>
         );
     }
