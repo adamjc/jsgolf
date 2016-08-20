26f47250034f8c88d3dcb980033a4440dd1710ab
adamjc <adamjc86@gmail.com>
2016-08-20 18:16:55 +0100

    Changed 'input length' to 'score'

--- client/source/js/views/exercise-input.jsx
+++ client/source/js/views/exercise-input.jsx
@@ -10,88 +10,87 @@ require('brace/mode/javascript')
 require('brace/theme/monokai')
 
 module.exports = React.createClass({
-    getInitialState() {
-        return {
-            answer: 'function () {\n\t return "Hello...";\n}'
-        }
-    },
+  getInitialState() {
+    return {
+      answer: 'function () {\n\t// Your code goes here...\n}'
+    }
+  },
 
-    componentDidMount() {
-        ResultStore.listen(this.onResult)
-    },
+  componentDidMount() {
+    ResultStore.listen(this.onResult)
+  },
 
-    componentWillUnmount() {
-        ResultStore.unlisten(this.onResult)
-    },
+  componentWillUnmount() {
+    ResultStore.unlisten(this.onResult)
+  },
 
-    onChange(answer) {
-        this.setState({
-            answer: answer
-        })
-    },
+  onChange(answer) {
+    this.setState({
+      answer: answer
+    })
+  },
 
-    onResult() {
-        this.setState({
-            awaitingResults: false
-        })
-    },
+  onResult() {
+    this.setState({
+      awaitingResults: false
+    })
+  },
 
-    handleClick() {
-        if (!this.state.answer) return
+  handleClick() {
+    if (!this.state.answer) return
 
-        ResultActions.fetchResults(this.props.exercise, this.state.answer)
+    ResultActions.fetchResults(this.props.exercise, this.state.answer)
 
-        this.setState({
-            awaitingResults: true
-        })
-    },
+    this.setState({
+      awaitingResults: true
+    })
+  },
 
     render() {
-        let awaitingResults
+      let awaitingResults
 
-        if (this.state.awaitingResults) {
-            awaitingResults =
-                    <div className="exercise-input__sent">
-                        <span className="glyphicon glyphicon-refresh" aria-hidden="true"></span>
-                    </div>
-        }
+      if (this.state.awaitingResults) {
+        awaitingResults =
+              <div className="exercise-input__sent">
+                <span className="glyphicon glyphicon-refresh" aria-hidden="true"></span>
+              </div>
+      }
 
-        return (
-            <div className="exercise-input col-sm-6">
-                <h3 className="exercise-input__header">
-                    Input Length:
-                    <span className="exercise-input__header-length"> {this.state.answer.length}</span>
-                </h3>
+      return (
+        <div className="exercise-input col-sm-6">
+          <h3 className="exercise-input__header">
+            Score: <span className="exercise-input__header-length">{this.state.answer.length}</span>
+          </h3>
 
-                <AceEditor
-                    className="exercise-input__editor"
-                    mode="javascript"
-                    theme="monokai"
-                    name="editor"
-                    height=""
-                    width=""
-                    onChange={this.onChange}
-                    value={this.state.answer}
-                />
+          <AceEditor
+            className="exercise-input__editor"
+            mode="javascript"
+            theme="monokai"
+            name="editor"
+            height=""
+            width=""
+            onChange={this.onChange}
+            value={this.state.answer}
+          />
 
-                <div className="exercise-input__button-wrapper">
-                    <button
-                        className="exercise-input__button btn btn-block"
-                        onClick={this.handleClick}>
-                        GO
-                    </button>
+          <div className="exercise-input__button-wrapper">
+            <button
+              className="exercise-input__button btn btn-block"
+              onClick={this.handleClick}>
+              GO
+            </button>
 
-                    <ReactCSSTransitionGroup
-                        transitionName="exericse-input-transition"
-                        transitionAppear={true}
-                        transitionAppearTimeout={500}
-                        transitionEnterTimeout={500}
-                        transitionLeaveTimeout={300}
-                    >
-                        {awaitingResults}
-                    </ReactCSSTransitionGroup>
-                </div>
-            </div>
-        )
+            <ReactCSSTransitionGroup
+              transitionName="exericse-input-transition"
+              transitionAppear={true}
+              transitionAppearTimeout={500}
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}
+            >
+              {awaitingResults}
+            </ReactCSSTransitionGroup>
+          </div>
+        </div>
+      )
     }
 })
