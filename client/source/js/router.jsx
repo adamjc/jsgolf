edc39a19dc85ddc68d49fedc0d2408ee0315eeca
adamjc <adamjc86@gmail.com>
2016-08-21 20:48:23 +0100

    Attempt to fix GA #2

--- client/source/js/router.jsx
+++ client/source/js/router.jsx
@@ -9,11 +9,11 @@ const ReactGA = require('react-ga')
 
 const UserActions = require('./actions/user-actions')
 
-ReactGA.initialize('UA-38765332-2');
+ReactGA.initialize('UA-38765332-2', { debug: true });
 
 function setAndSendStat(route, statType = 'pageView') {
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
+  ReactGA.set({ page: route })
+  ReactGA.pageview(route)
 }
 
 module.exports = React.createClass({
