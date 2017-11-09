d11485b90ac358da5eef5bc37f041ea2d9f5c76d
adamjc <adamjc86@gmail.com>
2017-11-09 07:53:50 +0000

    asyncify and cleanup get-exercises-list endpoint

--- server/api/get-exercises-list.js
+++ server/api/get-exercises-list.js
@@ -1,38 +1,26 @@
-const logger = require('../utils/logger')
 const ddbUtils = require('../utils/ddb-utils')
 const jwt = require('jsonwebtoken')
 const passwordUtils = require('../utils/password-utils')
+const fs = require('fs')
+const exerciseFilenames = fs.readdirSync('./server/exercises').map(filename => filename.split('.')[0])
 
-/* Returns the list of exercises available. */
-function getExercisesList (req, res) {
-  const jwtPromise = new Promise(resolve => {
-    try {
-      const authorized = jwt.verify(req.headers.authorization, passwordUtils.getSecret())
-      resolve(authorized)
-    } catch (e) {
-      logger.log('error', e)
-      res.json(buildExercisesList())
-    }
-  })
-
-  jwtPromise.then(auth => ddbUtils.getUser(auth.username))
-            .then(user => Object.keys(user.exercises).map(a => a.replace(new RegExp('_', 'g'), '-')))
-            .then(buildExercisesList)
-            .then(exercises => res.json(exercises))
+async function getExercisesList (req, res) {
+  const authorized = jwt.verify(req.headers.authorization, passwordUtils.getSecret())
+  const user = await ddbUtils.getUser(authorized.username)
+  const completedExercises = Object.keys(user.exercises).map(a => a.replace(new RegExp('_', 'g'), '-'))
+  res.json(buildExercisesList(completedExercises))
 }
 
 function buildExercisesList (completedExercises = []) {
-  const publicExercises = require('../utils/exercise-utils').publicExercises
-  const exercises = Object.keys(publicExercises).map(exerciseTitle => {
-    const exerciseFilename = publicExercises[exerciseTitle]
-    const userHasCompletedExercise = (completedExercises.indexOf(exerciseFilename) >= 0)
-    const exerciseCompleted = userHasCompletedExercise ? true : false
+  const exercises = exerciseFilenames.map(exerciseFilename => {
+    const exercise = require(`../exercises/${exerciseFilename}.js`)
+    const userHasCompletedExercise = completedExercises.includes(exerciseFilename) ? true : false
 
     return {
       id: exerciseFilename,
-      title: exerciseTitle,
+      title: exercise.title,
       url: `/exercises/${exerciseFilename}`,
-      completed: exerciseCompleted
+      completed: userHasCompletedExercise
     }
   })
 
