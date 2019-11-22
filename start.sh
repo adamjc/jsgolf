a354eecb78c0315ca1e0f4f465ea676e5919345c
adamjc <adamjc86@gmail.com>
2019-11-22 18:30:51 +0000

    Automate certbot

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@ yum install -y docker-ce
 yum install -y docker-compose
 
 cd /var/jsgolf-server
-docker-compose up -d --build
+docker-compose build --no-cache &&
+docker-compose up -d &&
+docker exec reverse_proxy certbot register --agree-tos --email adamjc86+jsgolf@gmail.com --no-eff-email
+docker exec reverse_proxy certbot -n --nginx --domains jsgolf.xyz,www.jsgolf.xyz
